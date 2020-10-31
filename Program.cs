aec0e8a362605da56669c22f4443aaa46cab5467
Deivide Silva <37459501+DeivideSilva@users.noreply.github.com>
2020-10-31 16:27:50 -0300

    Inserção de dados usando Migration

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    //Entidade
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public int Estoque { get; set; }
+    }
+
+    //Classe de contexto
+    public class AppDbContext : DbContext {
+        //mapeamento da entidade para a tabela
+        public DbSet <Produto> Produtos { get; set; }
+        //provedor e a string de conexão
+         protected  override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-FA2LSQI\\SQLEXPRESS;" + "Initial Catalog=Aula1DB;Integrated Security=true");
+           
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Produto>().HasData(new Produto 
+            { 
+                ProdutoId = 6,
+                Nome = "Etiquetas",
+                Preco = 11.30M,
+                Estoque = 13
+            });
+
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            using (var db = new AppDbContext())
+            {
+                //popular com dados dos produtos
+                SeedDataBase.SeedProdutos(db);
+
+                //exibir produtos
+                var produtos = db.Produtos.ToList();
+
+                 foreach(var p  in produtos)
+                {
+                    Console.WriteLine(p.Nome + "\t" + p.Preco.ToString("c"));
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SeedDataBase.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class SeedDataBase
+    {
+        public static void SeedProdutos(AppDbContext context)
+        {
+            if (!context.Produtos.Any())
+            {
+                var produtos = new List<Produto> 
+                { 
+                    new Produto {Nome = "Compasso", Preco=8.20M, Estoque=65},
+                    new Produto {Nome="Transferidor",Preco=10.00M, Estoque=70}
+                };
+                context.AddRange(produtos);
+                context.SaveChanges();
+
+            }
+            
+        }
+    }
+}
